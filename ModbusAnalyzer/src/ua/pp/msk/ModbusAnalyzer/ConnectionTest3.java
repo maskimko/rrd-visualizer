2701078dedde53a6b68c33b39585190d4fd7e6a2
Maksym Shkolnyi <maksym.shkolnyi@life.com.ua>
2013-08-27 18:06:15 +0300

    End of working day!

--- ModbusAnalyzer/src/ua/pp/msk/ModbusAnalyzer/ConnectionTest3.java
+++ ModbusAnalyzer/src/ua/pp/msk/ModbusAnalyzer/ConnectionTest3.java
@@ -17,30 +17,97 @@ public class ConnectionTest3 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		boolean keepAlive = false; //We don't keep alive TCP connection
+		boolean keepAlive = false; // We don't keep alive TCP connection
 		int devNum = 2;
 		ModbusFactory factory = new ModbusFactory();
 		IpParameters ipParm = new IpParameters();
 		ipParm.setHost("10.208.20.122");
 		ipParm.setPort(502);
 		ipParm.setEncapsulated(false);
-		
+
 		TcpMaster tm = new TcpMaster(ipParm, keepAlive);
-		ModbusLocator ml  = new ModbusLocator(devNum, RegisterRange.HOLDING_REGISTER, 4106, DataType.TWO_BYTE_INT_SIGNED);
-		ModbusLocator powerLocator = new ModbusLocator(devNum, RegisterRange.HOLDING_REGISTER, 4006, DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator currentScaleLocator = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4104,
+				DataType.TWO_BYTE_INT_SIGNED);
+		ModbusLocator voltageScaleLocator = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4105,
+				DataType.TWO_BYTE_INT_SIGNED);
+		ModbusLocator powerScaleLocator = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4106,
+				DataType.TWO_BYTE_INT_SIGNED);
+		ModbusLocator realPowerLocator = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4005,
+				DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator apparentPowerLocator = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4006,
+				DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator reactivePowerLocator = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4007,
+				DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator powerFactorLocator = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4008,
+				DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator frequencyLocator = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4012,
+				DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator currentLocatorA = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4019,
+				DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator currentLocatorB = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4020,
+				DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator currentLocatorC = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4021,
+				DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator currentLocatorN = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4022,
+				DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator voltageLocatorAB = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4029,
+				DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator voltageLocatorBC = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4030,
+				DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator voltageLocatorCA = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4031,
+				DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator voltageLocatorAN = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4032,
+				DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator voltageLocatorBN = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4032,
+				DataType.TWO_BYTE_INT_UNSIGNED);
+		ModbusLocator voltageLocatorCN = new ModbusLocator(devNum,
+				RegisterRange.HOLDING_REGISTER, 4034,
+				DataType.TWO_BYTE_INT_UNSIGNED);
 		try {
 			tm.init();
-		} catch (ModbusInitException mbie){
+		} catch (ModbusInitException mbie) {
 			mbie.printStackTrace();
 			return;
 		}
 		try {
-			int powerValue = (int) tm.getValue(powerLocator);
-			short someValue = (short) tm.getValue(ml);
-			float power = (float) (powerValue * Math.pow(10, someValue));
-			System.out.println("Scale value I got " + someValue + "\nPower value I got " + powerValue);
-			System.out.println("Power Value " + power);
-		
+			while (true) {
+				int realPowerValue = (int) tm.getValue(realPowerLocator);
+				int powerFactorValue = (int) tm.getValue(powerFactorLocator);
+				short powerScale = (short) tm.getValue(powerScaleLocator);
+				int voltageABValue = (int) tm.getValue(voltageLocatorAB);
+				short voltageScale = (short) tm.getValue(voltageScaleLocator);
+				int currentAValue = (int) tm.getValue(currentLocatorA);
+				short currentScale = (short) tm.getValue(currentScaleLocator);
+				float realPower = (float) (realPowerValue * Math.pow(10,
+						powerScale));
+				float voltageAB = (float) (voltageABValue * Math.pow(10,
+						voltageScale));
+				float currentA = (float) (currentAValue * Math.pow(10,
+						currentScale));
+				float powerFactor = (float) (powerFactorValue * Math.pow(10, powerScale));
+				System.out.println("Real Power  " + realPower);
+				System.out.println("Voltage AB " + voltageAB);
+				System.out.println("Current A " + currentA);
+				System.out.println("Power Factor "+ powerFactor);
+			}
+
 		} catch (ModbusTransportException mte) {
 			mte.printStackTrace();
 		} catch (ErrorResponseException ere) {
@@ -48,7 +115,7 @@ public class ConnectionTest3 {
 		} finally {
 			tm.destroy();
 		}
-		
+
 	}
 
 }
