02f6c0cf053ef07a5b7fb3dfd2baf2f763e1c18b
Maksym Shkolnyi <maksym.shkolnyi@life.com.ua>
2013-09-05 18:29:55 +0300

    Fixed SNMP Gauge32 to Integer32 Reactive Power Value

--- SNMPAgent/src/ua/pp/msk/SNMPAgent/RCUTable.java
+++ SNMPAgent/src/ua/pp/msk/SNMPAgent/RCUTable.java
@@ -18,8 +18,6 @@ import ua.pp.msk.ModbusAnalyzer.RCUPacketFloat;
 
 public class RCUTable extends DefaultMOTable {
 
-	
-	
 	public RCUTable(OID oid, MOTableIndex mti, MOColumn[] cols) {
 		super(oid, mti, cols);
 	}
@@ -40,85 +38,88 @@ public class RCUTable extends DefaultMOTable {
 	 * vars)); super.update(updateScope); }
 	 */
 
-
-	
-	
-	
 	/*
-	public void update(MOScope updateScope) {
-		if (canUpdate) {
-			Runnable judje = new UpdateJudje();
-			Thread updThread = new Thread(judje);
-			updThread.start();
-			
-			try {
-				RCUPacket rcup = rcua.askDevice();
-				Variable[] vars = toVar(rcup.getAll());
-				MOMutableTableModel model = (MOMutableTableModel) this
-						.getModel();
-				synchronized (model) {
-					model.clear();
-					model.addRow(new DefaultMOMutableRow2PC(new OID("1"), vars));
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			
-		}
-		
-		super.update(updateScope);
-	}
-	*/
-	
+	 * public void update(MOScope updateScope) { if (canUpdate) { Runnable judje
+	 * = new UpdateJudje(); Thread updThread = new Thread(judje);
+	 * updThread.start();
+	 * 
+	 * try { RCUPacket rcup = rcua.askDevice(); Variable[] vars =
+	 * toVar(rcup.getAll()); MOMutableTableModel model = (MOMutableTableModel)
+	 * this .getModel(); synchronized (model) { model.clear(); model.addRow(new
+	 * DefaultMOMutableRow2PC(new OID("1"), vars)); } } catch (Exception e) {
+	 * e.printStackTrace(); }
+	 * 
+	 * }
+	 * 
+	 * super.update(updateScope); }
+	 */
 
 	protected static RCUTable createStaticStatsTable() {
-		
+
 		MOTableSubIndex[] subIndexes = new MOTableSubIndex[] { new MOTableSubIndex(
 				SMIConstants.SYNTAX_GAUGE32) };
 		MOTableIndex indexDef = new MOTableIndex(subIndexes, false);
 		MOColumn[] columns = new MOColumn[RCUPacketFloat.units + 3];
 		int c = 0;
-		columns[c++] = new MOColumn(c, SMIConstants.SYNTAX_OCTET_STRING, MOAccessImpl.ACCESS_READ_ONLY); //Description
-		columns[c++] = new MOColumn(c, SMIConstants.SYNTAX_IPADDRESS, MOAccessImpl.ACCESS_READ_ONLY); //IP address of a device
-		columns[c++] = new MOColumn(c, SMIConstants.SYNTAX_GAUGE32, MOAccessImpl.ACCESS_READ_ONLY); // IP port of a device
-		while (c < columns.length){
-			columns[c++] = new MOColumn(c, SMIConstants.SYNTAX_GAUGE32,	MOAccessImpl.ACCESS_READ_ONLY);
+		columns[c++] = new MOColumn(c, SMIConstants.SYNTAX_OCTET_STRING,
+				MOAccessImpl.ACCESS_READ_ONLY); // Description
+		columns[c++] = new MOColumn(c, SMIConstants.SYNTAX_IPADDRESS,
+				MOAccessImpl.ACCESS_READ_ONLY); // IP address of a device
+		columns[c++] = new MOColumn(c, SMIConstants.SYNTAX_GAUGE32,
+				MOAccessImpl.ACCESS_READ_ONLY); // IP port of a device
+		while (c < columns.length) {
+			if (c == 16) {
+				columns[c++] = new MOColumn(c, SMIConstants.SYNTAX_INTEGER32,
+						MOAccessImpl.ACCESS_READ_ONLY);
+			} else {
+				columns[c++] = new MOColumn(c, SMIConstants.SYNTAX_GAUGE32,
+						MOAccessImpl.ACCESS_READ_ONLY);
+			}
 		}
-		
-		RCUTable deviceStats = new RCUTable(new OID(".1.3.6.1.4.1.2006.1.1.1"),	indexDef, columns);
-		MOMutableTableModel model = (MOMutableTableModel) deviceStats.getModel();
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseKyivRCUDevice((short)5));
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseKyivRCUDevice((short)4));
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseKyivRCUDevice((short)3));
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseKyivRCUDevice((short)2));
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseDnipropetrovskRCUDevice((short)2));
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseDnipropetrovskRCUDevice((short)3));
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseDonetskRCUDevice((short)2));
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseDonetskRCUDevice((short)3));
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseKharkivRCUDevice((short)2));
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseKharkivRCUDevice((short)3));
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseLvivRCUDevice((short)2));
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseLvivRCUDevice((short)3));
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseSimferopolRCUDevice((short)2));
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseSimferopolRCUDevice((short)3));
-		RCUModelFactory.addRCUDeviceToModel(model, RCUDevice.createBaseOdesaRCUDevice((short)2));
+
+		RCUTable deviceStats = new RCUTable(new OID(".1.3.6.1.4.1.2006.1.1.1"),
+				indexDef, columns);
+		MOMutableTableModel model = (MOMutableTableModel) deviceStats
+				.getModel();
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseKyivRCUDevice((short) 5));
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseKyivRCUDevice((short) 4));
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseKyivRCUDevice((short) 3));
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseKyivRCUDevice((short) 2));
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseDnipropetrovskRCUDevice((short) 2));
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseDnipropetrovskRCUDevice((short) 3));
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseDonetskRCUDevice((short) 2));
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseDonetskRCUDevice((short) 3));
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseKharkivRCUDevice((short) 2));
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseKharkivRCUDevice((short) 3));
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseLvivRCUDevice((short) 2));
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseLvivRCUDevice((short) 3));
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseSimferopolRCUDevice((short) 2));
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseSimferopolRCUDevice((short) 3));
+		RCUModelFactory.addRCUDeviceToModel(model,
+				RCUDevice.createBaseOdesaRCUDevice((short) 2));
 		deviceStats.setVolatile(true);
 		return deviceStats;
 	}
 
 	/*
-	class UpdateJudje implements Runnable {
-
-		public void run() {
-			if (canUpdate) {
-				canUpdate = false;
-				try {
-					Thread.sleep(5000);
-				} catch (InterruptedException ie) {
-				}
-				canUpdate = true;
-			}
-		}
-	}
-	*/
+	 * class UpdateJudje implements Runnable {
+	 * 
+	 * public void run() { if (canUpdate) { canUpdate = false; try {
+	 * Thread.sleep(5000); } catch (InterruptedException ie) { } canUpdate =
+	 * true; } } }
+	 */
 }
